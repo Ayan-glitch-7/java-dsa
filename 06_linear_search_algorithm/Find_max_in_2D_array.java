fef84317d63914e6328b321a2e6d6e4b22d29afa
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-04-17 17:11:21 +0000

    c

---
+++ 06_linear_search_algorithm/Find_max_in_2D_array.java
@@ -0,0 +1,28 @@
+
+public class Find_max_in_2D_array {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {23, 5, 7},
+            {3, 56, 76},
+            {32, 54, 45},
+            {43, 67, 9},
+            {21, 90, 71}
+        };
+
+        int ans = search(arr);     
+        System.out.println(ans);
+    }
+
+    static int search(int[][] arr) {
+        int max = arr[0][0];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        return max;
+    }
+}
