38a1b575401dcd2714b1bafc19db3c14c9b2d761
Mohd Ayan <mohdayan7245@gmail.com>
2026-06-11 12:59:05 +0000

    c

--- 08_binary_search_questions/Split_array_largest_sum.java
+++ 08_binary_search_questions/Split_array_largest_sum.java
@@ -12,7 +12,7 @@ public class Split_array_largest_sum {
             end += nums[i];
         }
 
-        // binary search
+        // binary search:
         while (start < end) {
             // try for the middle as the potential answer
             int mid = start + (end - start) / 2;
