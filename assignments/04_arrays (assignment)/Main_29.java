9afeecf24d9be866daa2ded2e3e4579a8c35fadc
Mohd Ayan <mohdayan7245@gmail.com>
2026-07-27 12:36:44 +0000

    c

---
+++ assignments/04_arrays (assignment)/Main_29.java
@@ -0,0 +1,45 @@
+/* LEETCODE (# 54) */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main_29 {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        int top = 0;
+        int right = matrix[0].length - 1;
+        int bottom = matrix.length - 1;
+        int left = 0;
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            for (int j = top; j <= bottom; j++) {
+                ans.add(matrix[j][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int k = right; k >= left; k--) {
+                    ans.add(matrix[bottom][k]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int l = bottom; l >= top; l--) {
+                    ans.add(matrix[l][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+}
