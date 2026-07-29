8a278095ad094fd8336346f6d0249e2ea8522178
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-07-29 22:32:06 +0530

    c

---
+++ assignments/04_arrays (assignment)/Main_31.java
@@ -0,0 +1,49 @@
+/* LEETCODE (# 885) */
+
+public class Main_31 {
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int[][] ans = new int[rows * cols][2];
+        int r = rStart;
+        int c = cStart;
+        int step = 1;
+        int i = 0;
+
+        ans[i++] = new int[] { r, c };
+
+        while (i < rows * cols) {
+
+            for (int j = 0; j < step; j++) {
+                c++;
+                if (r >= 0 && r < rows && c >= 0 && c < cols) {
+                    ans[i++] = new int[] { r, c };
+                }
+            }
+
+            for (int k = 0; k < step; k++) {
+                r++;
+                if (r >= 0 && r < rows && c >= 0 && c < cols) {
+                    ans[i++] = new int[] { r, c };
+                }
+            }
+
+            step++;
+
+            for (int l = 0; l < step; l++) {
+                c--;
+                if (r >= 0 && r < rows && c >= 0 && c < cols) {
+                    ans[i++] = new int[] { r, c };
+                }
+            }
+
+            for (int m = 0; m < step; m++) {
+                r--;
+                if (r >= 0 && r < rows && c >= 0 && c < cols) {
+                    ans[i++] = new int[] { r, c };
+                }
+            }
+
+            step++;
+        }
+        return ans;
+    }
+}
