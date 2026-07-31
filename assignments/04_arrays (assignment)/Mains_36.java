30f2843b55f80e5b0073e9a99ed8a804a7e30dc0
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-07-31 23:06:43 +0530

    c

---
+++ assignments/04_arrays (assignment)/Mains_36.java
@@ -0,0 +1,25 @@
+/* LEETCODE (# 189) */
+
+public class Mains_36 {
+    public void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        if (k >= n) {
+            k = k % n;
+        }
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = nums[n - k + i];
+        }
+
+        for (int j = 0; j < n - k; j++) {
+            ans[j + k] = nums[j];
+        }
+
+        for (int l = 0; l < n; l++) {
+            nums[l] = ans[l];
+        }
+    }
+}
