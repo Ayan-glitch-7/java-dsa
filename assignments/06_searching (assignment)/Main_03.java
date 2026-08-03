638f0885e4df3a15f40f6b258857cf786f30eeec
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-08-03 11:36:05 +0530

    c

---
+++ assignments/06_searching (assignment)/Main_03.java
@@ -0,0 +1,22 @@
+/* LEETCODE (# 278) */
+
+public class Main_03 {
+    // public int firstBadVersion(int n) {
+
+    //     int start = 0;
+    //     int end = n;
+
+    //     while (start <= end) {
+    //         int mid = start + (end - start) / 2;
+
+    //         if (isBadVersion(mid) == true && isBadVersion(mid - 1) == false) {
+    //             return mid;
+    //         } else if (isBadVersion(mid) == false) {
+    //             start = mid + 1;
+    //         } else {
+    //             end = mid - 1;
+    //         }
+    //     }
+    //     return -1;
+    // }
+}
