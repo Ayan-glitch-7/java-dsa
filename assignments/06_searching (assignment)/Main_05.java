c9736340c469fb5cf020838c3003a8ccf08f3001
Mohd Ayan <mohdayan7245@gmail.com>
2026-08-03 10:24:23 +0000

    c

---
+++ assignments/06_searching (assignment)/Main_05.java
@@ -0,0 +1,23 @@
+/* LEETCODE (#367) */
+
+public class Main_05 {
+    // public boolean isPerfectSquare(int num) {
+
+    //     int start = 0;
+    //     int end = num;
+
+    //     while (start <= end) {
+    //         int mid = start + (end - start) / 2;
+    //         long ans = (long) mid * mid;
+
+    //         if (ans < num) {
+    //             start = mid + 1;
+    //         } else if (ans > num) {
+    //             end = mid - 1;
+    //         } else {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+}
