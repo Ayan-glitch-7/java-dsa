f2571522a49dba63c2c57df26d7c23fc3fbafc53
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-08-04 14:17:23 +0530

    c

---
+++ assignments/06_searching (assignment)/Main_07.java
@@ -0,0 +1,22 @@
+/* LEETCODE (#744) */
+
+public class Main_07 {
+    // public char nextGreatestLetter(char[] letters, char target) {
+
+    //     int start = 0;
+    //     int end = letters.length - 1;
+
+    //     while (start <= end) {
+    //         int mid = start + (end - start) / 2;
+
+    //         if (letters[mid] > target) {
+    //             end = mid - 1;
+    //         } else if (letters[mid] < target) {
+    //             start = mid + 1;
+    //         } else {
+    //             return letters[mid + 1];
+    //         }
+    //     }
+    //     return letters[0];
+    // }
+}
---
+++ assignments/06_searching (assignment)/Main_08.java
@@ -0,0 +1,21 @@
+/* LEETCODE (#1539) */
+
+public class Main_08 {
+    // public int findKthPositive(int[] arr, int k) {
+
+    //     int start = 0;
+    //     int end = arr.length - 1;
+
+    //     while (start <= end) {
+    //         int mid = start + (end - start) / 2;
+    //         int missing = arr[mid] - (mid + 1);
+
+    //         if (missing < k) {
+    //             start = mid + 1;
+    //         } else {
+    //             end = mid - 1;
+    //         }
+    //     }
+    //     return start + k;
+    // }
+}
