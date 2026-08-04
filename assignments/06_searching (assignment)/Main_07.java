5a6266ceaaf15b75dcbd8a6c6de30d63b6214cb2
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-08-04 14:37:33 +0530

    c

--- assignments/06_searching (assignment)/Main_07.java
+++ assignments/06_searching (assignment)/Main_07.java
@@ -11,12 +11,10 @@ public class Main_07 {
 
     //         if (letters[mid] > target) {
     //             end = mid - 1;
-    //         } else if (letters[mid] < target) {
-    //             start = mid + 1;
     //         } else {
-    //             return letters[mid + 1];
+    //             start = mid + 1;
     //         }
     //     }
-    //     return letters[0];
+    //     return letters[start % letters.length];
     // }
 }
