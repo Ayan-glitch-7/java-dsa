20bbc718fe54badd01c496d87462d3d181f5537e
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-08-04 14:48:01 +0530

    c

---
+++ assignments/06_searching (assignment)/Main_09.java
@@ -0,0 +1,22 @@
+/* LEETCODE (#35) */
+
+public class Main_09 {
+    // public int searchInsert(int[] nums, int target) {
+
+    //     int start = 0;
+    //     int end = nums.length - 1;
+
+    //     while (start <= end) {
+    //         int mid = start + (end - start);
+
+    //         if (nums[mid] < target) {
+    //             start = mid + 1;
+    //         } else if (nums[mid] > target) {
+    //             end = mid - 1;
+    //         } else {
+    //             return mid;
+    //         }
+    //     }
+    //     return start;
+    // }
+}
