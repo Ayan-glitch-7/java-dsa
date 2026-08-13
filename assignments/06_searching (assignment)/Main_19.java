6c7fc0d354b2668bf3ac65a87c0b95f5a5204daf
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-08-13 09:50:06 +0530

    c

---
+++ assignments/06_searching (assignment)/Main_19.java
@@ -0,0 +1,28 @@
+/* LEETCODE (#540) */
+
+public class Main_19 {
+    // public int singleNonDuplicate(int[] nums) {
+
+    //     int start = 0;
+    //     int end = nums.length - 1;
+
+    //     while (start <= end) {
+    //         int mid = start + (end - start) / 2;
+
+    //         if (mid == 0 || mid == nums.length - 1) {
+    //             return nums[mid];
+    //         } else if (nums[mid] == nums[mid - 1] && mid % 2 == 0) {
+    //             end = mid - 1;
+    //         } else if (nums[mid] == nums[mid - 1] && mid % 2 != 0) {
+    //             start = mid + 1;
+    //         } else if (nums[mid] == nums[mid + 1] && mid % 2 == 0) {
+    //             start = mid + 1;
+    //         } else if (nums[mid] == nums[mid + 1] && mid % 2 != 0) {
+    //             end = mid - 1;
+    //         } else {
+    //             return nums[mid];
+    //         }
+    //     }
+    //     return -1;
+    // }
+}
