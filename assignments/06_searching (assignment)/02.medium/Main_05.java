ca17b603b898979561c869ee2134754a43be9c30
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-08-13 15:28:56 +0530

    c

---
+++ assignments/06_searching (assignment)/02.medium/Main_05.java
@@ -0,0 +1,20 @@
+/* LEETCODE (#153) */
+
+public class Main_05 {
+    // public int findMin(int[] nums) {
+
+    //     int start = 0;
+    //     int end = nums.length - 1;
+
+    //     while (start < end) {
+    //         int mid = start + (end - start) / 2;
+
+    //         if (nums[mid] < nums[end]) {
+    //             end = mid;
+    //         } else if (nums[mid] > nums[end]) {
+    //             start = mid + 1;
+    //         }
+    //     }
+    //     return nums[start];
+    // }
+}
