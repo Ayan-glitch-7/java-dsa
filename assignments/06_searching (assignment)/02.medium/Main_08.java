2de8534325a7b59d18e3ee2b43f354767b87613a
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-08-31 23:19:20 +0530

    c

---
+++ assignments/06_searching (assignment)/02.medium/Main_08.java
@@ -0,0 +1,14 @@
+/* LEETCODE (#754) */
+
+public class Main_08 {
+    // public int reachNumber(int target) {
+    //     int moves = 0;
+    //     int total = 0;
+    //     target = Math.abs(target);
+    //     while(total < target || (total-target) % 2 != 0){
+    //         moves++;
+    //         total += moves; 
+    //     }
+    //     return moves;
+    // }
+}
